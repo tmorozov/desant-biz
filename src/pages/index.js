f82bc7e907076b7f551e193d7000cde404e73c6a
Tymur Morozov <radist@desant.biz>
2022-10-05 14:05:56 +0300

    Dima

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,39 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import {DynamicLayout} from "../components/dynamic-layout";
-import {Preview} from "../components/preview";
-import {MoreArticles} from "../components/more-articles";
-import {homePageArticles, homePageMoreArticles} from "../../data/articles";
+// import { DynamicLayout } from "../components/dynamic-layout"
+// import { Preview } from "../components/preview"
+// import { MoreArticles } from "../components/more-articles"
+// import { homePageArticles, homePageMoreArticles } from "../../data/articles"
+import Dima from "../../static/images/stories/img_ls/image010.jpg"
 
-const exampleStructure = homePageArticles.map(row => {
-  return row.map(({title, introtext, path, fulltext}) => (
-  <Preview 
-    title={title} 
-    introtext={introtext} 
-    path={path} 
-    fulltext={fulltext}
-  />)
-  );
-})
+// const exampleStructure = homePageArticles.map(row => {
+//   return row.map(({ title, introtext, path, fulltext }) => (
+//     <Preview
+//       title={title}
+//       introtext={introtext}
+//       path={path}
+//       fulltext={fulltext}
+//     />
+//   ))
+// })
 
-
-const IndexPage = ({location}) => (
+const IndexPage = ({ location }) => (
   <Layout location={location}>
     <SEO title="Home" />
-    <h2>Страйкбольная команда Сфинкс. Город Львов.</h2>
-    
-    <DynamicLayout structure={exampleStructure} />
-    <MoreArticles articles={homePageMoreArticles}/>
+    <h2>Страйкбольна команда Сфінкс. Львів.</h2>
+
+    <div style={{ textAlign: "center" }}>
+      <img
+        style={{ border: "10px solid black" }}
+        src={Dima}
+        title="Загинув наш товариш Діма"
+        alt="Загинув наш товариш Діма"
+      />
+    </div>
+
+    {/* <DynamicLayout structure={exampleStructure} /> */}
+    {/* <MoreArticles articles={homePageMoreArticles} /> */}
   </Layout>
 )
 
